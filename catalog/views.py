b1099580d816cb182c9f001682e3eceb847286fc
tuhamaina-blip <tuhamaina@gmail.com>
2026-09-11 14:46:33 +0300

    fix: prevent duplicate author records on creation

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions, viewsets
 from django.contrib.auth.models import User
 from .models import Book, Author, Genre
 from .serializers import RegisterSerializer, BookSerializer, AuthorSerializer, GenreSerializer
-
+from rest_framework.response import Response
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -25,6 +25,12 @@ class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        author, created = Author.objects.get_or_create(name=name, defaults={'bio': request.data.get('bio', '')})
+        serializer = self.get_serializer(author)
+        return Response(serializer.data, status=201 if created else 200)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
