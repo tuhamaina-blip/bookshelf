1bd2150772c9b74427a10c4067545a4762ffcb67
tuhamaina-blip <tuhamaina@gmail.com>
2026-09-11 14:01:08 +0300

    feat: add register and jwt login endpoints

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
