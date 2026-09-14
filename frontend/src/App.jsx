19e553750af428e5ccdff468a63cc040f0114f71
tuhamaina-blip <tuhamaina@gmail.com>
2026-09-14 13:06:35 +0300

    style: wrap Routes component with Navbar for improved layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,26 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
-import './App.css';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-   <div className="bg-stone-50 min-h-screen text-stone-800">
-  <Routes>
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/" element={<Home />} />
-  </Routes>
-</div>
+    <div className="bg-stone-50 min-h-screen text-stone-800">
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
