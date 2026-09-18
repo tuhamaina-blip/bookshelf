373ba4869ed9191684737bdc8edcdbe6c4a263d6
tuhamaina-blip <tuhamaina@gmail.com>
2026-09-18 12:49:35 +0300

    refactor: restructure to shared catalog with UserBook shelf and public Review model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Author, Genre, Book
+from .models import Author, Genre, Book, UserBook, Review
 
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book)
+admin.site.register(UserBook)
+admin.site.register(Review)
