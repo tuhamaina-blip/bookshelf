da4fd8198d6773911a76251faec333503dca5a02
tuhamaina-blip <tuhamaina@gmail.com>
2026-09-20 18:08:33 +0300

    feat: register Comment model in admin and ensure Review model is properly registered

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, UserBook, Review
+from .models import Author, Genre, Book, UserBook, Review, Comment
 
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book)
 admin.site.register(UserBook)
 admin.site.register(Review)
+admin.site.register(Comment)
