cb9a66706fc05b9b39f44b9cce1a94dcc649e55c
tuhamaina-blip <tuhamaina@gmail.com>
2026-09-25 14:32:32 +0300

    feat: add ownership checks for review update and delete actions

--- catalog/views.py
+++ catalog/views.py
@@ -83,6 +83,17 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def update(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            return Response({'detail': 'You can only edit your own reviews.'}, status=403)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            return Response({'detail': 'You can only delete your own reviews.'}, status=403)
+        return super().destroy(request, *args, **kwargs)
 
 class RecommendationsView(APIView):
     permission_classes = [permissions.IsAuthenticated]
